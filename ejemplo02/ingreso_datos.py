85111a3298bc5aa9db05a957d1878031da84ed4a
ErickGaona <Erickgaona625@gmail.com>
2025-05-20 09:56:43 -0500

    Actualizando ingresos

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import csv
 
 # se importa la clase(s) del 
 # archivo genera_tablas
@@ -16,28 +17,27 @@ engine = create_engine(cadena_base_datos)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Cargar datos de Jugador
+# Cargar datos de Club
 with open('datos_clubs.txt', newline='', encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='|')
+    reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
-        club = session.query(Club).filter_by(nombre=row['nombre_club']).one()
-        jugador = Jugador(
+        club = Club(
             nombre=row['nombre'],
-            dorsal=int(row['dorsal']),
-            posicion=row['posicion'],
-            club=club
+            deporte=row['deporte'],
+            fundacion=int(row['fundacion'])
         )
-        session.add(jugador)
+        session.add(club)
 
 session.commit()
-print(f"📦 Datos cargados desde el CSV: {len(lista_datos)} registros")
+
+#print(f"📦 Datos cargados desde el TXT: {len(lista_datos)} registros")
 
 
 # Cargar datos de Jugador
 with open('datos_jugadores.txt', newline='', encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter='|')
+    reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
-        club = session.query(Club).filter_by(nombre=row['nombre_club']).one()
+        club =  session.query(Club).filter_by(nombre="LDU").one()
         jugador = Jugador(
             nombre=row['nombre'],
             dorsal=int(row['dorsal']),
@@ -48,51 +48,4 @@ with open('datos_jugadores.txt', newline='', encoding='utf-8') as csvfile:
 
 session.commit()
 
-
-
-
-# se crea un objetos de tipo Club 
-club1 = Club(nombre="Barcelona", deporte="Fútbol", \
-        fundacion=1920)
-
-club2 = Club(nombre="Emelec", deporte="Fútbol", \
-        fundacion=1930)
-
-club3 = Club(nombre="Liga de Quito", deporte="Fútbol", \
-        fundacion=1940)
-
-# Se crean objeto de tipo Jugador
-#
-jugador1 = Jugador(nombre ="Damian Diaz", dorsal=10, posicion="mediocampo", \
-        club=club1)
-jugador2 = Jugador(nombre ="Matias Oyola", dorsal=18, posicion="mediocampo", \
-        club=club1)
-jugador3 = Jugador(nombre ="Dario Aymar", dorsal=2, posicion="defensa", \
-        club=club1)
-
-
-jugador4 = Jugador(nombre ="Oscar Bagui", dorsal=6, posicion="defensa", \
-        club=club2)
-jugador5 = Jugador(nombre ="Romario Caicedo", dorsal=11, posicion="mediocampo", \
-        club=club2)
-
-
-jugador6 = Jugador(nombre ="Adrián Gabbarini", dorsal=1, posicion="arquero", \
-        club=club3)
-jugador7 = Jugador(nombre ="Cristian Martinez", dorsal=9, posicion="delantero", \
-        club=club3)
-
-# se agrega los objetos
-# a la sesión
-session.add(club1)
-session.add(club2)
-session.add(club3)
-session.add(jugador1)
-session.add(jugador2)
-session.add(jugador3)
-session.add(jugador4)
-session.add(jugador5)
-session.add(jugador6)
-
-# se confirma las transacciones
-session.commit()
+#print(f"📦 Datos cargados desde el TXT: {len(lista_datos)} registros")
